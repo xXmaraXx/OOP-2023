20445df366fc3207e2761f346c5a49a473b0fb62
Mara <mara.popescu03@gmail.com>
2023-02-22 19:28:32 +0200

    lab1

--- l1/Source.cpp
+++ l1/Source.cpp
@@ -1,45 +1,57 @@
 #include "header.h"
 
-int Sum(int a, float b) { return a - b; }
-int Dif(char a, int b) { return a / b; }
-int Mul(long a, int b) { return a + b; }
-char Div(int a, int b) { return a * b; }
+int Sum(int a, int b) { 
+	return a + b; 
+}
+int Dif(int a, int b) { 
+	return a - b; 
+}
+int Mul(int a, int b) { 
+	return a * b; 
+}
+int Div(int a, int b) { 
+	return a / b; 
+}
 
-int main(int argc, char* argv[])
+int main()
 {
-	char input[7] = "---H***E+++L+++L///O---P+++O/+-**O---";
-	func Operatori[4] = {Sum, Dif, 65, Mul, 0, Div};
-	int S, V;
-	Content x = 15;
-	double idx;
+    char input[] = "---H***E+++L+++L///O---P+++O/+-**O---";
+	func Operatori[] = {Sum, Dif, Mul, Div};
+    int S = 0,V;
+    Content x{};
+    int idx;
 
-	for (i = 0; i < strlen(input); i++)
+	for (int i = 0; i < strlen(input); i++)
 	{
-		switch (input[i] - 42)
+        switch (input[i] - 42)
 		{
 			case INMULTIRE:
 				idx = 2;
 				x.p1 = 3;
 				x.p2 = 3;
+                break;
 			case SUMA:
 				idx = 0;
 				x.p1 = 7;
 				x.p2 = 5;
+                break;
 			case DIFERENTA:
 				idx = 1;
 				x.p1 = 10;
 				x.p2 = 1;
+                break;
 			case IMPARTIRE:
 				idx = 3;
 				x.p1 = 8;
 				x.p2 = 4;
+                break;
 		}
 
 		S = S + Operatori[idx](x.p1, x.p2);
 	}
 
 	//S=337
-	printf("S = %c\n", S);
+	printf("S = %d\n", S);
 
 	return 0;
 }
