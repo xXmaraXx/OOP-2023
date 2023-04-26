20b2ec0ae66e218f6cef0a311d7e9f0e20b69aaf
Mara <mara.popescu03@gmail.com>
2023-04-26 15:29:59 +0300

    laboratorul 8

---
+++ lab8/lab8/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <queue>
+#include <string>
+#include <sstream>
+#include <vector>
+
+class Compare {
+public:
+	bool operator()(const std::pair<std::string, int>& a, const std::pair<std::string, int>& b) {
+		if (a.second != b.second) {
+			return a.second < b.second;
+		}
+		else {
+			return a.first > b.first;
+		}
+	}
+};
+
+int main() {
+
+	std::ifstream inputFile("input.txt");
+	
+	std::string phrase="";
+	if (inputFile.is_open()) {
+		std::getline(inputFile, phrase);
+	}
+	else {
+		std::cerr << "Failed to open input file." << std::endl;
+        return 0;
+	}
+
+	std::map<std::string, int> wordCount;
+	std::string word="";
+
+	for (auto c : phrase) {
+		if (c == ' ' || c == ',' || c == '?' || c == '!' || c == '.') {
+			if (!word.empty()) {
+				for (auto& ch : word) {
+					ch = std::tolower(ch);
+				}
+				wordCount[word]++;
+				word.clear();
+			}
+		}
+		else {
+			word.push_back(c);
+		}
+	}
+
+	if (!word.empty()) {
+		for (auto& ch : word) {
+			ch = std::tolower(ch);
+		}
+		wordCount[word]++;
+	}
+
+	std::priority_queue<std::pair<std::string, int>, std::vector<std::pair<std::string, int>>, Compare> pq;
+
+	for (const auto& pair : wordCount) {
+		pq.push(pair);
+	}
+
+	while (!pq.empty()) {
+		std::cout << pq.top().first << " => " << pq.top().second << std::endl;
+		pq.pop();
+	}
+
+	
+	return 0;
+}
